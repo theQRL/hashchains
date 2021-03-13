393a6e354545f114aa87b3f763e834c50c592ff4
JP Lomas <jp@theqrl.org>
2021-03-13 12:57:07 +0000

    Refactor into Class-based code and rewrite tests

--- src/index.js
+++ src/index.js
@@ -1,43 +1,55 @@
-const keccak256 = require ('keccak256');
+/* eslint max-classes-per-file: 0 */
 
-function browserRandom(count) {
-  const nativeArr = new Uint8Array(count);
-  const crypto = window.crypto || window.msCrypto;
-  crypto.getRandomValues(nativeArr);
-  return [].slice.call(nativeArr);
+const keccak256 = require('keccak256')
+
+class HashChain {
+  constructor(hashRoot, hashFunction, length) {
+    this.hashRoot = hashRoot
+    this.hashFunction = hashFunction || 'keccak256'
+    this.length = length || 64
+    const hc = []
+    hc.push(this.hashRoot)
+    for (let i = 0; i < this.length; i += 1) {
+      if (this.hashFunction === 'keccak256') {
+        hc.push(keccak256(`${hc[i]}`).toString('hex'))
+      } else {
+        throw new Error('hash function not implemented')
+      }
+    }
+    this.chain = hc
+    return hc
+  }
 }
 
-function makeHashChain(hashRoot) {
-  const hc = [];
-  hc.push(hashRoot);
-  for (let i = 0; i < 64; i += 1) {
-    hc.push(keccak256(`${hc[i]}`).toString("hex"));
+class HashChains {
+  constructor(mnemonic, numberToCreate, index, hashFunction, length) {
+    this.hashFunction = hashFunction || 'keccak256'
+    this.length = length || 64
+    const startingIndex = index || 0
+    const chainsToMake = numberToCreate || 2
+    const hashChains = []
+    const hashChainRoots = []
+    let hashRoot = null
+    for (let i = 0; i < chainsToMake; i += 1) {
+      if (this.hashFunction === 'keccak256') {
+        hashRoot = keccak256(`${mnemonic}${startingIndex + i}`).toString('hex')
+      } else {
+        throw new Error('hash function not implemented')
+      }
+      hashChainRoots.push(hashRoot)
+      const hc = new HashChain(hashRoot)
+      hashChains.push({
+        index: startingIndex + i,
+        hashRoot,
+        hashReveal: hc[64],
+        hashchain: hc,
+      })
+    }
+    this.chains = hashChains
   }
-  return hc;
-}
-
-function makeHashChains(mnemonic, number, index) {
-  const startingIndex = index || 0;
-  const chainsToMake = number || 2;
-  const hashChains = [];
-  const hashChainRoots = [];
-  for (let i = 0; i < chainsToMake; i += 1) {
-    const hashRoot = keccak256(`${mnemonic}${startingIndex + i}`).toString(
-      "hex"
-    );
-    hashChainRoots.push(hashRoot);
-    const hc = makeHashChain(hashRoot);
-    hashChains.push({
-      index: startingIndex + i,
-      hashRoot,
-      hashReveal: hc[64],
-      hashchain: hc,
-    });
-  }
-  return hashChains;
 }
 
 module.exports = {
-  makeHashChain,
-  makeHashChains
+  HashChains,
+  HashChain
 }
